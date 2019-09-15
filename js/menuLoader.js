06ee69e49c774d580dcd4f507151db837254adcb
Jathan Hibbs <jathan@tencentsolutions.com>
2019-09-14 18:22:24 -0700

    Menu can change manually

--- js/menuLoader.js
+++ js/menuLoader.js
@@ -1,20 +1,27 @@
-//access menu json
-$.getJSON("menu/menu.json", function (json)
+/**
+ * loads a specific menu from menu.json
+ * @param {any} type which menu to load
+ */
+function loadMenu(type)
 {
-  //loops through every item in the entrees object
-  for (var i = 0; i < json.entrees.length; i++)
+  //access menu json
+  $.getJSON("menu/menu.json", function (json)
   {
-    //html to insert for each food item
-    var foodItem = "<div class='row'><div class='cell food'>" + json.entrees[i].name + "</div><div class='cell price'>" + json.entrees[i].price + "</div></div ><div class='row'><div class='cell desc'>" + json.entrees[i].description + "</div></div>";
+    //loops through every item in the entrees object
+    for (var i = 0; i < json[type].length; i++)
+    {
+      //html to insert for each food item
+      var foodItem = "<div class='row'><div class='cell food'>" + json[type][i].name + "</div><div class='cell price'>" + json[type][i].price + "</div></div ><div class='row'><div class='cell desc'>" + json[type][i].description + "</div></div>";
 
-    //alternates adding items to the left or right
-    if (i % 2 == 0)
-    {
-      $(".left").append(foodItem);
+      //alternates adding items to the left or right
+      if (i % 2 == 0)
+      {
+        $(".left").append(foodItem);
+      }
+      else
+      {
+        $(".right").append(foodItem);
+      }
     }
-    else
-    {
-      $(".right").append(foodItem);
-    }
-  }
-});
+  });
+}
---
+++ js/startup.js
@@ -0,0 +1,1 @@
+loadMenu("entrees");
